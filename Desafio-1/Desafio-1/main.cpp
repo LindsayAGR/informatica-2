4dd6710d3f0a5177ea15565c3c60c09aa297a1ec
Jaider Restrepo <jaider.restrepo1@udea.edu.co>
2025-09-27 12:41:04 -0500

    Rotacion XOR

--- Desafio-1/Desafio-1/main.cpp
+++ Desafio-1/Desafio-1/main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-using namespace std;
+// Constantes
+#define MAX_FRAGMENTO 8192         // Tamaño máximo de pista (texto conocido)
+#define MAX_SALIDA (6*1024*1024)   // 6 MB para salida descomprimida
+#define PREVIEW_MAX 80             // cantidad de chars en preview
+const char BASE_DIR[] = "C:/Users/JAIDER RESTREPO/Documents/desafio1/";
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+// Rotación y XOR
+
+unsigned char rotarDerecha(unsigned char b, int n) {
+    return (unsigned char)((b >> n) | (b << (8 - n)));
+}
+unsigned char aplicarXOR(unsigned char b, unsigned char K) {
+    return (unsigned char)(b ^ K);
 }
